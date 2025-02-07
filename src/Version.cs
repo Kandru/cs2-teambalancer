2f5c00a396154226ce45fa97a1616c402b5f07da
Kalle <kalle@kandru.de>
2025-02-07 12:48:52 +0100

    bump version to 0.0.9

--- src/Version.cs
+++ src/Version.cs
@@ -1,11 +1,9 @@
 using CounterStrikeSharp.API.Core;
-using CounterStrikeSharp.API.Core.Attributes.Registration;
-using CounterStrikeSharp.API.Modules.Commands;
 
 namespace TeamBalancer
 {
     public partial class TeamBalancer : BasePlugin
     {
-        public override string ModuleVersion => "0.0.8";
+        public override string ModuleVersion => "0.0.9";
     }
 }

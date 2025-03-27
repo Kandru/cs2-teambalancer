9ecc0217a1244d82699893fe2a404145fdae80f6
Kalle <kalle@kandru.de>
2025-03-27 17:42:47 +0100

    bump version to 0.0.13

--- src/Version.cs
+++ src/Version.cs
@@ -4,6 +4,6 @@ namespace TeamBalancer
 {
     public partial class TeamBalancer : BasePlugin
     {
-        public override string ModuleVersion => "0.0.12";
+        public override string ModuleVersion => "0.0.13";
     }
 }
